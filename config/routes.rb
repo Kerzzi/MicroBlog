7264cb0017f69fef1430664c40e273b21d791c87
Kerzzi <kerzzi@outlook.com>
2017-04-14 22:23:09 +0800

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  def hello
+    render html: "hello, world!"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
